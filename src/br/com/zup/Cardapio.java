c818c92e34bd3b75dad909aaad7767da1a681209
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-28 23:43:12 -0300

    Estrutura da Classe Cardapio

--- src/br/com/zup/Cardapio.java
+++ src/br/com/zup/Cardapio.java
@@ -1,4 +1,31 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cardapio {
+    private List<Prato> prato = new ArrayList<>();
+
+    public Cardapio(List<Prato> prato) {
+        this.prato = prato;
+    }
+
+    public List<Prato> getPrato() {
+        return prato;
+    }
+
+    public void setPrato(List<Prato> prato) {
+        this.prato = prato;
+    }
+
+    public void addPrato (Prato pratoAdd){
+        prato.add(pratoAdd);
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder retorno = new StringBuilder();
+      retorno.append("O prato do cardapio é/: "+prato);
+      return retorno.toString();
+    }
 }
