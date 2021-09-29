592da8b5b11fd10b8d4aabef0d58c67d3b19e285
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-28 23:30:11 -0300

    Retirando o Atributo quantidade e melhorando o toString

--- src/br/com/zup/Ingredientes.java
+++ src/br/com/zup/Ingredientes.java
@@ -3,14 +3,12 @@ package br.com.zup;
 
 public class Ingredientes {
     private String addIngredientes;
-    private int qtd_ingredientes;
 
     public Ingredientes() {
     }
 
-    public Ingredientes(String addIngredientes, int qtd_ingredientes) {
+    public Ingredientes(String addIngredientes) {
         this.addIngredientes = addIngredientes;
-        this.qtd_ingredientes = qtd_ingredientes;
     }
 
     public String getAddIngredientes() {
@@ -21,19 +19,11 @@ public class Ingredientes {
         this.addIngredientes = addIngredientes;
     }
 
-    public int getQtd_ingredientes() {
-        return qtd_ingredientes;
-    }
-
-    public void setQtd_ingredientes(int qtd_ingredientes) {
-        this.qtd_ingredientes = qtd_ingredientes;
-    }
 
     @Override
     public String toString() {
-        return "Ingredientes{" +
-                "addIngredientes='" + addIngredientes + '\'' +
-                ", qtd_ingredientes=" + qtd_ingredientes +
-                '}';
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Nome do ingrediente: "+ addIngredientes);
+        return retorno.toString();
     }
 }
